3278ef1323f41ee0638f72ec295ae18b5f2fe38c
Victor K <dev@victorkarasev.ru>
2023-03-27 01:53:41 +0700

    refactor code

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 const square = document.getElementById("square");
 const again = document.getElementById("again");
-const NUMBERS = 15;
-let ELEM_ID, NULL_ID;
+let NULL_ID = null;
 let WAIT_ANIM = false;
 
-const getArray = () => square.getElementsByTagName("b");
+const getTags = () => square.getElementsByTagName("b");
 
 const beginGame = () => {
-  square.innerHTML = ""; //clear "frame"
-   //fill fields - simple <b> tag
-  for (let i = 0; i < NUMBERS + 1; i++) {square.innerHTML += "<b></b>";}
-  const mass = getArray(); //mass of fields
-  for (let i = 0; i < NUMBERS; i++) {
-    mass[i].innerHTML = Math.floor(Math.random() * NUMBERS + 1); //fill in random num
-    for (let j = 1; j <= i; j++) {
-      if (mass[j - 1].innerHTML == mass[i].innerHTML) //checking of clones
-      {
-        mass[i].innerHTML = Math.floor(Math.random() * NUMBERS + 1);
-        j = 0;
-      } //fill in another num
-    }
+  const initial = [];
+  const refactor = [];
+  while (initial.length <= 15) { initial.push(initial.length) };
+  while (initial.length > 0) {
+    const element = initial.splice(~~(Math.random() * initial.length), 1)[0];
+    if (element === 0) NULL_ID = refactor.length;
+    refactor.push(`<b>${element || ''}</b>`);
   }
-  NULL_ID = NUMBERS;
+  square.innerHTML = refactor.join('');
 };
 
 const move = (fromId, toId, dir) => { //move "brick" from-to, direction
+  if (!dir) return;
   WAIT_ANIM = true;
-  const mass = getArray(); //mass of fields
+  const mass = getTags(); //mass of fields
   mass[fromId].classList.add("--"+dir);
   //get time for timeout from css
   const time = getComputedStyle(mass[fromId]).transitionDuration.replace(/[a-z]/,'') * 1000;
@@ -41,8 +35,8 @@ const move = (fromId, toId, dir) => { //move "brick" from-to, direction
 }
 
 const checkWinner = () => { //checking the end of the game
-  const mass = getArray(); //mass of fields
-  for (var i = NUMBERS; i > 0; i--) {
+  const mass = getTags(); //mass of fields
+  for (var i = mass.length-1; i > 0; i--) {
     if (i != mass[i-1].innerHTML) break;
     if (i == 1) {
       alert("winner");
@@ -55,22 +49,26 @@ const clickHandler = (e) => { //move cheking
   const { target: el } = e;
   if (WAIT_ANIM || el.childElementCount) return;
   if (el.innerHTML) {
-    const mass = getArray(); //mass of fields
-    for (let i = 0; i <= NUMBERS; i++) {
+    let ELEM_ID = null;
+    const mass = getTags(); //mass of fields
+    for (let i = 0; i < mass.length; i++) {
       if (mass[ELEM_ID = i].innerHTML == el.innerHTML) break;
     }
-    if (ELEM_ID % 4 == NULL_ID % 4) { //in column
-      if (ELEM_ID - 4 == NULL_ID) return move(ELEM_ID, NULL_ID, "up");
-      if (ELEM_ID + 4 == NULL_ID) return move(ELEM_ID, NULL_ID, "down");
+    if (!ELEM_ID) return;
+    const direction = () => {
+      if (ELEM_ID % 4 == NULL_ID % 4) { //in column
+        if (ELEM_ID - 4 == NULL_ID) return "up";
+        if (ELEM_ID + 4 == NULL_ID) return "down";
+      }
+      if (~~(ELEM_ID / 4) == ~~(NULL_ID / 4)) { // in row
+        if ((ELEM_ID % 4) - 1 == NULL_ID % 4) return "left";
+        if ((ELEM_ID % 4) + 1 == NULL_ID % 4) return "right";
+      } 
     }
-    if (Math.floor(ELEM_ID / 4) == Math.floor(NULL_ID / 4)) { // in row
-      if ((ELEM_ID % 4) - 1 == NULL_ID % 4) return move(ELEM_ID, NULL_ID, "left");
-      if ((ELEM_ID % 4) + 1 == NULL_ID % 4) return move(ELEM_ID, NULL_ID, "right");
-    }
+    move(ELEM_ID, NULL_ID, direction())
   }
 }
 
 again.onclick = beginGame;
 square.onclick = clickHandler;
-
 beginGame();
