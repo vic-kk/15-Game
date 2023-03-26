dbcfbe44674b5401402b474d8eb8ac7c24da63df
Victor K <dev@victorkarasev.ru>
2023-03-27 00:28:40 +0700

    refactor

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const square = document.getElementById("square");
 const again = document.getElementById("again");
 const NUMBERS = 15;
-let elId, nullId, el, mass;
-let waitAnim = false;
+let ELEM_ID, NULL_ID;
+let WAIT_ANIM = false;
 
-const begin = () => {
+const getArray = () => square.getElementsByTagName("b");
+
+const beginGame = () => {
   square.innerHTML = ""; //clear "frame"
    //fill fields - simple <b> tag
   for (let i = 0; i < NUMBERS + 1; i++) {square.innerHTML += "<b></b>";}
-  mass = square.getElementsByTagName("b"); //mass of fields
+  const mass = getArray(); //mass of fields
   for (let i = 0; i < NUMBERS; i++) {
     mass[i].innerHTML = Math.floor(Math.random() * NUMBERS + 1); //fill in random num
     for (let j = 1; j <= i; j++) {
@@ -19,11 +21,12 @@ const begin = () => {
       } //fill in another num
     }
   }
-  nullId = NUMBERS;
+  NULL_ID = NUMBERS;
 };
 
 const move = (fromId, toId, dir) => { //move "brick" from-to, direction
-  waitAnim = true;
+  WAIT_ANIM = true;
+  const mass = getArray(); //mass of fields
   mass[fromId].classList.add("--"+dir);
   //get time for timeout from css
   const time = getComputedStyle(mass[fromId]).transitionDuration.replace(/[a-z]/,'') * 1000;
@@ -31,41 +34,43 @@ const move = (fromId, toId, dir) => { //move "brick" from-to, direction
     mass[fromId].classList.remove("--"+dir);
     mass[toId].innerHTML = mass[fromId].innerHTML;
     mass[fromId].innerHTML = "";
-    nullId = fromId;
-    waitAnim = false;
+    NULL_ID = fromId;
+    WAIT_ANIM = false;
     checkWinner();
   }, time);
 }
 
 const checkWinner = () => { //checking the end of the game
+  const mass = getArray(); //mass of fields
   for (var i = NUMBERS; i > 0; i--) {
     if (i != mass[i-1].innerHTML) break;
     if (i == 1) {
       alert("winner");
-      begin();
+      beginGame();
     }
   }
 }
 
 const clickHandler = (e) => { //move cheking
   const { target: el } = e;
-  if (waitAnim || el.childElementCount) return;
+  if (WAIT_ANIM || el.childElementCount) return;
   if (el.innerHTML) {
+    const mass = getArray(); //mass of fields
     for (let i = 0; i <= NUMBERS; i++) {
-      if (mass[elId = i].innerHTML == el.innerHTML) break;
+      if (mass[ELEM_ID = i].innerHTML == el.innerHTML) break;
     }
-    if (elId % 4 == nullId % 4) { //in column
-      if (elId - 4 == nullId) return move(elId, nullId, "up");
-      if (elId + 4 == nullId) return move(elId, nullId, "down");
+    if (ELEM_ID % 4 == NULL_ID % 4) { //in column
+      if (ELEM_ID - 4 == NULL_ID) return move(ELEM_ID, NULL_ID, "up");
+      if (ELEM_ID + 4 == NULL_ID) return move(ELEM_ID, NULL_ID, "down");
     }
-    if (Math.floor(elId / 4) == Math.floor(nullId / 4)) { // in row
-      if ((elId % 4) - 1 == nullId % 4) return move(elId, nullId, "left");
-      if ((elId % 4) + 1 == nullId % 4) return move(elId, nullId, "right");
+    if (Math.floor(ELEM_ID / 4) == Math.floor(NULL_ID / 4)) { // in row
+      if ((ELEM_ID % 4) - 1 == NULL_ID % 4) return move(ELEM_ID, NULL_ID, "left");
+      if ((ELEM_ID % 4) + 1 == NULL_ID % 4) return move(ELEM_ID, NULL_ID, "right");
     }
   }
 }
 
-again.onclick = begin;
+again.onclick = beginGame;
 square.onclick = clickHandler;
 
-begin();
+beginGame();
