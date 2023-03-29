66fa0782835cc7ae164a9d2e41b63c9533e28b63
Victor K <dev@victorkarasev.ru>
2023-03-29 22:26:15 +0700

    refactor

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 const square = document.getElementById("square");
 const again = document.getElementById("again");
-let NULL_ID = 0;
 let WAIT_ANIMATION = false;
 
 const getArrayOfB = () => [...square?.getElementsByTagName("b")];
 
 const beginNewGame = () => {
   const initial = [];
-  while (initial.length <= 15) { initial.push(initial.length); }
   const refactor = [];
+  while (initial.length <= 15) { initial.push(initial.length); }
   while (initial.length > 0) {
-    const element = initial.splice(~~(Math.random() * initial.length), 1)[0];
-    if (element === 0) NULL_ID = refactor.length;
-    refactor.push(`<b data-brick=${refactor.length}>${element || ''}</b>`);
+    const element = initial.splice(~~(Math.random() * initial.length), 1).at(0);
+    refactor.push(`<b data-brick-id=${refactor.length}>${element || ''}</b>`);
   }
   square.innerHTML = refactor.join('');
 };
@@ -22,7 +20,7 @@ const checkWinner = () => {
   for (var i = mass.length-1; i > 0; i--) {
     if (i != mass[i-1].innerHTML) break;
     if (i == 1) alert("winner");
-  }
+  };
 };
 
 const moveBrick = (from_id, to_id, direction) => {
@@ -35,27 +33,28 @@ const moveBrick = (from_id, to_id, direction) => {
     mass[to_id].innerHTML = mass[from_id].innerHTML;
     mass[from_id].classList.remove(`--${direction}`);
     mass[from_id].innerHTML = '';
-    NULL_ID = from_id;
     WAIT_ANIMATION = false;
     checkWinner();
   }, delay);
 };
 
-const clickHandler = ({ target, target: { dataset, tagName } } = e) => {
-  if (WAIT_ANIMATION || tagName != 'B' && !dataset.hasOwnProperty('brick')) return;
-  const ELEM_ID = getArrayOfB().indexOf(target);
+const clickHandler = ({ target: { dataset, tagName } } = e) => {
+  if (WAIT_ANIMATION) return;
+  if (tagName != 'B' && !dataset.hasOwnProperty('brickId')) return;
+  const elem_id = +dataset.brickId;
+  const null_id = +getArrayOfB().find(item => item.innerHTML === '').dataset.brickId;
   const moveDirection = (() => {
-    if (ELEM_ID % 4 == NULL_ID % 4) { // in column
-      if (ELEM_ID - 4 == NULL_ID) return "up";
-      if (ELEM_ID + 4 == NULL_ID) return "down";
-    }
-    if (~~(ELEM_ID / 4) == ~~(NULL_ID / 4)) { // in row
-      if ((ELEM_ID % 4) - 1 == NULL_ID % 4) return "left";
-      if ((ELEM_ID % 4) + 1 == NULL_ID % 4) return "right";
-    }
+    if (elem_id % 4 == null_id % 4) { // in column
+      if (elem_id - 4 == null_id) return "up";
+      if (elem_id + 4 == null_id) return "down";
+    };
+    if (~~(elem_id / 4) == ~~(null_id / 4)) { // in row
+      if ((elem_id % 4) - 1 == null_id % 4) return "left";
+      if ((elem_id % 4) + 1 == null_id % 4) return "right";
+    };
     return null;
   })();
-  if (moveDirection) moveBrick(ELEM_ID, NULL_ID, moveDirection);
+  if (moveDirection) moveBrick(elem_id, null_id, moveDirection);
 };
 
 again.onclick = beginNewGame;
